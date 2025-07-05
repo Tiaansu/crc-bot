f00a6fd287ca1cf1bfa6981c09cd7e2df051681d
Tiaansu <dev.marlon.cabading@gmail.com>
2025-07-05 15:27:49 +0800

    clean drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,4 @@
 import { defineConfig } from 'drizzle-kit';
-import { config } from 'dotenv';
-
-config({
-    path: './.env',
-});
-
-console.log(process.env.DATABASE_URL);
 
 export default defineConfig({
     dialect: 'postgresql',
