de081585cfc296f7108f11cea19d1fd02c3007e1
Tiaansu <dev.marlon.cabading@gmail.com>
2025-08-03 17:26:55 +0800

    forgot to add api key on better fetch

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,5 +1,6 @@
 import { API_URL } from '@/utils/constants';
 import { createFetch, createSchema } from '@better-fetch/fetch';
+import { envParseString } from '@skyra/env-utilities';
 import { infoSchemaArray } from './schemas/info';
 
 const schema = createSchema({
@@ -10,6 +11,9 @@ const schema = createSchema({
 
 export const $fetch = createFetch({
     baseURL: API_URL,
+    headers: {
+        'jstudio-key': envParseString('JSTUDIO_API_KEY'),
+    },
     schema: schema,
     throw: true,
 });
